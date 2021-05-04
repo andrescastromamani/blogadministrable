6f5cd7d8f1507ec76924f20c7e6305f51f0b1cb5
Andrés Castro Mamani <castroandresrm@gmail.com>
2021-05-04 17:20:36 -0400

    Filtramos los posts por Categoria

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
@@ -15,5 +16,12 @@ class PostController extends Controller
         $similares = Post::where('category_id',$post->category_id)
                             ->where('status',2)->where('id','!=',$post->id)->latest('id')->take(4)->get();
         return view('posts.show',compact('post','similares'));
+    }
+
+    public function category(Category $category){
+        $posts = Post::where('category_id',$category->id)
+                    ->where('status',2)
+                    ->latest('id')->paginate(6);
+        return view('posts.category', compact('posts','category'));
     }
 }
