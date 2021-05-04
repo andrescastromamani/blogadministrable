8fd8e83853ffd5eab7d0e77c11f37566b226e067
Andrés Castro Mamani <castroandresrm@gmail.com>
2021-05-04 18:05:45 -0400

    Filtramos los posts por etiquetas

--- resources/views/components/card-post.blade.php
+++ resources/views/components/card-post.blade.php
@@ -11,7 +11,7 @@
     </div>
     <div class="px-6 pt-4 pb-2">
         @foreach($post->tags as $tag)
-            <a class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm text-gray-700 mr-2" href="">{{$tag->name}}</a>
+            <a class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm text-gray-700 mr-2" href="{{route('posts.tag',$tag)}}">{{$tag->name}}</a>
         @endforeach
     </div>
 </article>
