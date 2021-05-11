0bf9960c73fbcb0e97470a44b855a6ae2c1ba3aa
Andrés Castro Mamani <castroandresrm@gmail.com>
2021-05-10 23:24:02 -0400

    Agregamos partial form en categorias y active enlace en la barra de Navegacion

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -10,20 +10,7 @@
     <div class="card">
         <div class="card-body">
             {!! Form::open(['route' => 'admin.categories.store']) !!}
-            <div class="form-group">
-                {!! Form::label('name', 'Nombre') !!}
-                {!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el Nombre']) !!}
-                @error('name')
-                    <span class="text-red">{{ $message }}</span>
-                @enderror
-            </div>
-            <div class="form-group">
-                {!! Form::label('slug', 'Slug') !!}
-                {!! Form::text('slug', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el Slug', 'readonly']) !!}
-                @error('slug')
-                    <span class="text-red">{{ $message }}</span>
-                @enderror
-            </div>
+            @include('admin.categories.partials.form')
             {!! Form::submit('Crear Categoria', ['class' => 'btn btn-primary']) !!}
             {!! Form::close() !!}
         </div>
--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -17,20 +17,7 @@
     <div class="card">
         <div class="card-body">
             {!! Form::model($category, ['route' => ['admin.categories.update', $category], 'method' => 'PUT']) !!}
-            <div class="form-group">
-                {!! Form::label('name', 'Nombre') !!}
-                {!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el Nombre']) !!}
-                @error('name')
-                    <span class="text-red">{{ $message }}</span>
-                @enderror
-            </div>
-            <div class="form-group">
-                {!! Form::label('slug', 'Slug') !!}
-                {!! Form::text('slug', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el Slug', 'readonly']) !!}
-                @error('slug')
-                    <span class="text-red">{{ $message }}</span>
-                @enderror
-            </div>
+            @include('admin.categories.partials.form')
             {!! Form::submit('Actualizar Categoria', ['class' => 'btn btn-primary']) !!}
             {!! Form::close() !!}
         </div>
