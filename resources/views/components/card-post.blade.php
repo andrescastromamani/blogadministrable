6c14d6e15f2f941cdd454b2edfdde05ce7836260
Andrés Castro Mamani <castroandresrm@gmail.com>
2021-05-11 21:27:55 -0400

    Agregamos una imagen por defecto en los posts

--- resources/views/components/card-post.blade.php
+++ resources/views/components/card-post.blade.php
@@ -1,12 +1,16 @@
 @props(['post'])
 <article class="mb-8 bg-white shadow-lg rounded-lg overflow-hidden">
-    <img class="w-full h-72 object-cover object-center" src="{{Storage::url($post->image->url)}}" alt="image">
+    @if($post->image)
+        <img class="w-full h-72 object-cover object-center" src="{{Storage::url($post->image->url)}}" alt="image">
+    @else
+        <img class="w-full h-72 object-cover object-center" src="https://lh3.googleusercontent.com/proxy/T8PsHVAV7RkewJbWFkmnnX2Rmm0F5EDA5E4_9cfZ98leOWcq8mcId1o27tIV9R-QVUkqmDwDEoeHlh8RvbNAy-ji2eLVbYONuhlNIM4wlpv33eGewxSZ" alt="image">
+    @endif
     <div class="px-6 py-8">
         <h1 class="font-bold text-xl mb-2">
             <a href="{{route('posts.show',$post)}}">{{$post->name}}</a>
         </h1>
         <div class="text-gray-700 text-base">
-            {{$post->extract}}
+            {!! $post->extract !!}
         </div>
     </div>
     <div class="px-6 pt-4 pb-2">
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,14 +1,18 @@
 <x-app-layout>
     <div class="container py-8">
         <h1 class="text-4xl font-bold text-gray-600">{{$post->name}}</h1>
-        <div class="text-lg text-gray-500 mb-2">{{$post->extract}}</div>
+        <div class="text-lg text-gray-500 mb-2">{!! $post->extract!!}</div>
         <div class="grid grid-cols-1 lg:grid-cols-3">
             <div class="md:col-span-2">
                 <figure>
-                    <img class="w-full h-80 object-cover object-center" src="{{Storage::url($post->image->url)}}" alt="image">
+                    @if($post->image)
+                        <img class="w-full h-80 object-cover object-center" src="{{Storage::url($post->image->url)}}" alt="image">
+                    @else
+                        <img class="w-full h-80 object-cover object-center" src="https://lh3.googleusercontent.com/proxy/T8PsHVAV7RkewJbWFkmnnX2Rmm0F5EDA5E4_9cfZ98leOWcq8mcId1o27tIV9R-QVUkqmDwDEoeHlh8RvbNAy-ji2eLVbYONuhlNIM4wlpv33eGewxSZ" alt="image">
+                    @endif
                 </figure>
                 <div class="text-base text-gray-500 mt-4">
-                    {{$post->body}}
+                    {!! $post->body !!}
                 </div>
             </div>
             <aside class="lg:ml-6">
@@ -17,7 +21,11 @@
                     @foreach($similares as $similar)
                         <li class="mb-4">
                             <a class="flex" href="{{route('posts.show', $similar)}}">
-                                <img class="w-30 h-20 object-cover object-center" src="{{Storage::url($similar->image->url)}}" alt="">
+                                @if($similar->image)
+                                    <img class="w-30 h-20 object-cover object-center" src="{{Storage::url($similar->image->url)}}" alt="">
+                                @else
+                                    <img class="w-30 h-20 object-cover object-center" src="https://lh3.googleusercontent.com/proxy/T8PsHVAV7RkewJbWFkmnnX2Rmm0F5EDA5E4_9cfZ98leOWcq8mcId1o27tIV9R-QVUkqmDwDEoeHlh8RvbNAy-ji2eLVbYONuhlNIM4wlpv33eGewxSZ" alt="">
+                                @endif
                                 <span class="ml-2 text-gray-600">{{$similar->name}}</span>
                             </a>
                         </li>
