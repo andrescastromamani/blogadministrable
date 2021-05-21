9e61750474cd1a7ff9703a672f19c9ff63f67a1b
Andrés Castro Mamani <castroandresrm@gmail.com>
2021-05-20 21:33:35 -0400

    Agregamos el componente para mostrar los Usuarios

---
+++ resources/views/livewire/admin/users-index.blade.php
@@ -0,0 +1,41 @@
+<div class="card">
+    <div class="card-header">
+        <input wire:model="search" class="form-control" placeholder="Ingrese el nombre del Usuario">
+    </div>
+    @if($users->count())
+        <div class="card-body">
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Email</th>
+                    <th colspan="2">Accion</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach ($users as $user)
+                    <tr>
+                        <th scope="row">{{ $user->id }}</th>
+                        <td scope="row">{{ $user->name }}</td>
+                        <td scope="row">{{ $user->email }}</td>
+                        <td width="10px"><a class="btn btn-primary btn-sm" href="{{ route('admin.users.edit', $user) }}">editar</a></td>
+                        <td width="10px">
+                            <form action="{{ route('admin.users.destroy', $user) }}" method="post">
+                                @csrf
+                                @method('delete')
+                                <button class="btn btn-danger btn-sm" type="submit">Eliminar</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+        <div class="card-footer">{{$users->links()}}</div>
+    @else
+        <div class="card-header">
+            <strong>No se encontraron registros...</strong>
+        </div>
+    @endif
+</div>
