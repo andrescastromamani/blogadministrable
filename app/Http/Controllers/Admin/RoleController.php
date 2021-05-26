6618fec52eb3637537123bb9570ca5eeb59f6324
Andrés Castro Mamani <castroandresrm@gmail.com>
2021-05-25 21:22:19 -0400

    Mostramos el Formulario de Creacion de usuarios

--- app/Http/Controllers/Admin/RoleController.php
+++ app/Http/Controllers/Admin/RoleController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
 class RoleController extends Controller
@@ -16,7 +17,8 @@ class RoleController extends Controller
 
     public function create()
     {
-        return view('admin.roles.create');
+        $permissions = Permission::all();
+        return view('admin.roles.create',compact('permissions'));
     }
 
     public function store(Request $request)
--- resources/views/admin/roles/create.blade.php
+++ resources/views/admin/roles/create.blade.php
@@ -7,13 +7,27 @@
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
-@stop
-
-@section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
-@stop
-
-@section('js')
-    <script> console.log('Hi!'); </script>
+    <div class="card">
+        <div class="card-body">
+            {!! Form::open(['route' => 'admin.roles.store']) !!}
+                <div class="form-group">
+                    {!! Form::label('name', 'Nombre') !!}
+                    {!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el Nombre del Rol']) !!}
+                    @error('name')
+                    <span class="text-red">{{ $message }}</span>
+                    @enderror
+                </div>
+                <h2 class="h3">Lista de Permisos</h2>
+                @foreach($permissions as $permission)
+                    <div>
+                        <label for="">
+                            {!! Form::checkbox('permissions[]',$permission->id,null,['class'=>'mr-1']) !!}
+                            {{$permission->description}}
+                        </label>
+                    </div>
+                @endforeach
+            {!! Form::submit('Crear Categoria', ['class' => 'btn btn-primary']) !!}
+            {!! Form::close() !!}
+        </div>
+    </div>
 @stop
