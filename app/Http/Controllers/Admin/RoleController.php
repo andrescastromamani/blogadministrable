034abeff2ec416136d357e9d86e8244bbbe10529
Andrés Castro Mamani <castroandresrm@gmail.com>
2021-05-25 20:54:14 -0400

    Mostramos las lista de todos los roles

--- app/Http/Controllers/Admin/RoleController.php
+++ app/Http/Controllers/Admin/RoleController.php
@@ -4,17 +4,19 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
 
 class RoleController extends Controller
 {
     public function index()
     {
-        //
+        $roles = Role::paginate(10);
+        return view('admin.roles.index',compact('roles'));
     }
 
     public function create()
     {
-        //
+        return view('admin.roles.create');
     }
 
     public function store(Request $request)
@@ -22,22 +24,22 @@ class RoleController extends Controller
         //
     }
 
-    public function show($id)
+    public function show(Role $role)
+    {
+        return view('admin.roles.show',compact('role'));
+    }
+
+    public function edit(Role $role)
+    {
+        return view('admin.roles.edit',compact('role'));
+    }
+
+    public function update(Request $request, Role $role)
     {
         //
     }
 
-    public function edit($id)
-    {
-        //
-    }
-
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    public function destroy($id)
+    public function destroy(Role $role)
     {
         //
     }
---
+++ resources/views/admin/roles/index.blade.php
@@ -0,0 +1,48 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Lista de Roles</h1>
+@stop
+
+@section('content')
+    <div class="card">
+        <div class="card-header">
+            <a class="btn btn-primary float-right" href="{{ route('admin.roles.create') }}">Agregar Rol +</a>
+        </div>
+        <div class="card-body">
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nombre</th>
+                    <th colspan="2">Accion</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach ($roles as $rol)
+                    <tr>
+                        <th scope="row">{{ $rol->id }}</th>
+                        <td colspan="2">{{ $rol->name }}</td>
+                        <td width="10px">
+                            <a class="btn btn-warning btn-sm" href="{{ route('admin.roles.edit', $rol) }}">editar</a>
+                        </td>
+                        <td width="10px">
+                            <form action="{{ route('admin.roles.destroy', $rol) }}" method="post">
+                                @csrf
+                                @method('delete')
+                                <button class="btn btn-danger btn-sm" type="submit">Eliminar</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+        <div class="mt-4">
+            {{$roles->links()}}
+        </div>
+    </div>
+@stop
+
